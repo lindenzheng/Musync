7fb3d5225827300742a6da3bcda300b017ad0ab3
UniquelyBad <jeremysgao@gmail.com>
2023-06-29 22:54:56 -0400

    formatting, playing note, next note, notechecker(not working)

--- Musync/MorseView/MorseView.swift
+++ Musync/MorseView/MorseView.swift
@@ -11,102 +11,136 @@ import UIKit
 import AVFoundation
 
 var player: AVAudioPlayer?
+var noteNum = 1
+var letters: [String] = ["A", "B", "C", "D", "E", "F", "G"]
+var correct = false
 
 //play certain note
 func playSound(note: String) {
     
-    let path = Bundle.main.path(forResource: "\(note).mp3", ofType:nil)!
+    let path = Bundle.main.path(forResource: "\(note)4.mp3", ofType:nil)!
     let url = URL(fileURLWithPath: path)
     
     do {
         player = try AVAudioPlayer(contentsOf: url)
         player?.play()
     } catch {
-        print("error 2")
+        print("morse playSound audio file not found")
+    }
+}
+
+func noteChecker (input: String) {
+    if (input == letters[noteNum-1]){
+        correct = true
+        print("notechecker true")
+    } else {
+        correct = false
+        print("notechecker false")
     }
 }
 
 struct MorseView: View {
-
     
-
     var body: some View {
-
+        
         VStack {
-
+            
             Text("Morse")
-            
-                .font(.largeTitle)
-            
-                .fontWeight(.bold)
-            
-                .padding()
-            
+            .font(.largeTitle)
+            .fontWeight(.bold)
+            .padding()
             
             //buttons
-            Button(action: {
-                print("tapped!")
-            }, label: {
-            Text("a")
-                .foregroundColor(.red)
-                .frame(width: 200, height: 40)
+            
+            Button(
+                action: {
+                    print("notetapped!")
+                    playSound(note: letters[noteNum-1])
+                }, label: {
+                    Text("Note")
+                        .foregroundColor(.black)
+                        .frame(width: 400, height: 200)
+                })
+            
+            Button(
+                action: {
+                    print("tapped!")
+                    noteChecker(input: "A")
+                }, label: {
+                    Text("a")
+                        .foregroundColor(correct ? .black : .green)
+                        .frame(width: 200, height: 40)
             })
             
-            Button(action: {
-                print("tapped!")
-            }, label: {
-            Text("b")
-                .foregroundColor(.red)
-                .frame(width: 200, height: 40)
+            Button(
+                action: {
+                    print("tapped!")
+                }, label: {
+                    Text("b")
+                        .foregroundColor(.red)
+                        .frame(width: 200, height: 40)
             })
             
-            Button(action: {
-                print("tapped!")
-            }, label: {
-            Text("c")
-                .foregroundColor(.red)
-                .frame(width: 200, height: 40)
+            Button(
+                action: {
+                    print("tapped!")
+                }, label: {
+                    Text("c")
+                        .foregroundColor(.red)
+                        .frame(width: 200, height: 40)
             })
             
-            Button(action: {
-                print("tapped!")
-            }, label: {
-            Text("d")
-                .foregroundColor(.red)
-                .frame(width: 200, height: 40)
+            Button(
+                action: {
+                    print("tapped!")
+                }, label: {
+                    Text("d")
+                        .foregroundColor(.red)
+                        .frame(width: 200, height: 40)
             })
             
-            Button(action: {
-                print("tapped!")
-            }, label: {
-            Text("e")
-                .foregroundColor(.red)
-                .frame(width: 200, height: 40)
+            Button(
+                action: {
+                    print("tapped!")
+                }, label: {
+                    Text("e")
+                        .foregroundColor(.red)
+                        .frame(width: 200, height: 40)
             })
             
-            Button(action: {
-                print("tapped!")
-            }, label: {
-            Text("f")
-                .foregroundColor(.red)
-                .frame(width: 200, height: 40)
+            Button(
+                action: {
+                    print("tapped!")
+                }, label: {
+                    Text("f")
+                        .foregroundColor(.red)
+                        .frame(width: 200, height: 40)
             })
             
-            Button(action: {
-                print("tapped!")
-            }, label: {
-            Text("g")
-                .foregroundColor(.red)
-                .frame(width: 200, height: 40)
+            Button(
+                action: {
+                    print("tapped!")
+                }, label: {
+                    Text("g")
+                        .foregroundColor(.red)
+                        .frame(width: 200, height: 40)
+            })
+            
+            Button(
+                action:{
+                    print("nexted")
+                    noteNum = Int.random(in:1..<8)
+                    
+                }, label: {
+                    Text("next")
+                        .foregroundColor(.red)
+                        .frame(width: 200, height: 40)
             })
             
         }
 
-
     }
-
     
-
 }
 
 
